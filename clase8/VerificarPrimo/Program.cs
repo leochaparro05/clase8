236e374fc3f0a789dd824169f126be7d592b81d6
leochaparro05 <leochaparro150@gmail.com>
2024-05-16 16:10:18 -0300

    inicio ejercicio de verificar primo

---
+++ clase8/VerificarPrimo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarPrimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero");
+            int num;
+            num = Convert.ToInt16(Console.ReadLine());
+            int cont0 = 0;
+            int b = num / 2;
+            for (int i = 2; i <= b; i++)
+            {
+                if (num % i != 0)
+                    cont0++;
+            }
+            if (cont0 == 0)
+                Console.WriteLine("ES PRIMO");
+            else Console.WriteLine("NO ES PRIMO");
+            Console.ReadKey();
+        }
+       
+    }
+}
