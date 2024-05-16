fd23917b4ebeb390148da2b6120f7c600c29dd6f
leochaparro05 <leochaparro150@gmail.com>
2024-05-16 17:24:46 -0300

    generar primos

---
+++ clase8/generar primos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generar_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("solicitar desde y hasta ");
+            int desde = Convert.ToInt32(Console.ReadLine());
+            int hasta = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = desde; i < hasta; i++)
+            {
+                int num = i;
+                int divisor = 0;
+         
+                for (int n = 2; i <= num / 2; i++)
+                {
+
+                    if (num % n == 0)
+                    {
+
+                        divisor++;
+
+
+                    }
+                    if (divisor == 0)
+                        Console.WriteLine($"{num}");
+                }
+              
+
+                
+             
+            }
+        }
+    }
+}
